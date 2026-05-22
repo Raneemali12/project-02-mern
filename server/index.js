645d742ac266df4f06af1e933d06d8eeb9e2d4d4
raneemali12 <2438601@std.hu.edu.jo>
2026-05-22 10:46:22 +0000

    Fix session cookie syntax

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: true, sameSite: 'none'
+  cookie: { secure: true, sameSite: 'none'}
 }));
 
 app.use(logger);
